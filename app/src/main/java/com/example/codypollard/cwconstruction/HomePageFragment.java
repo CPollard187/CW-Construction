cb2ce23d69ed733e7ed7232c6744a4e0df0ce8b1
WilsonPaz-Abdulsalam <34166254+WilsonPaz-Abdulsalam@users.noreply.github.com>
2018-11-14 18:21:37 -0500

    Revert "Home page"

--- app/src/main/java/com/example/codypollard/cwconstruction/HomePageFragment.java
+++ app/src/main/java/com/example/codypollard/cwconstruction/HomePageFragment.java
@@ -1,15 +1,12 @@
 package com.example.codypollard.cwconstruction;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 
 /**
@@ -29,9 +26,6 @@ public class HomePageFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
-    //ImageButton twitButton;
 
     private OnFragmentInteractionListener mListener;
 
@@ -70,57 +64,7 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
-        ImageButton twitButton = (ImageButton) view.findViewById(R.id.twitButton);
-        ImageButton faceButton = (ImageButton) view.findViewById(R.id.faceButton);
-        ImageButton instaButton = (ImageButton) view.findViewById(R.id.instaButton);
-
-        //Twitter Button That Will Send you to the twitter website
-//        twitButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-//                    startActivity(intent);
-//                }
-//                else{
-//                    Toast.makeText(getContext(),
-//                            "Incorrect software",
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-
-        //Facebook Button That Will Send you to the facebook website
-//        faceButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-//                    startActivity(intent);
-//                }
-//                else{
-//                    Toast.makeText(getContext(),
-//                            "Incorrect software",
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-        //Instagram Button That Will Send you to the instagram website
-//        instaButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-//                    startActivity(intent);
-//                }
-//                else{
-//                    Toast.makeText(getContext(),
-//                            "Incorrect software",
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-
-        return view;
-
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
