9fcf72ef99959ccdbc2c35d1bccd2667b7aadc4a
Cody Pollard <codypollard01@stclairconnect.ca>
2018-11-22 14:08:12 -0500

    #9 Added the code and strings needed for the material drop down

--- app/src/main/java/com/example/codypollard/cwconstruction/QuoteFragment.java
+++ app/src/main/java/com/example/codypollard/cwconstruction/QuoteFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -109,6 +111,13 @@ public class QuoteFragment extends Fragment {
 
             });
         }
+        Spinner spinner = (Spinner) findViewById(R.id.materialDropDown);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.R.array.material_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
