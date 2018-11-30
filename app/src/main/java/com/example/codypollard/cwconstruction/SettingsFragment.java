30b4c4fe845ffc237f7aa3d2fb000412224c78e1
WilsonPaz-Abdulsalam <wilson.paz-abdulsalam53@stclairconnect.ca>
2018-11-30 14:47:23 -0500

    #6 Added the Settings Screen with prefernces

---
+++ app/src/main/java/com/example/codypollard/cwconstruction/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.example.codypollard.cwconstruction;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState,
+                                    String rootKey) {
+
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+
+}
